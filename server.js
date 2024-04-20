14e724dabe035307228b4fe6323f3462f28754d8
Adarsh <adarshsingh0427@gmail.com>
2024-04-21 03:03:35 +0530

    added attendence for student

--- server.js
+++ server.js
@@ -23,34 +23,63 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "./client/public")));
 
 app.get("/student/dashboard", (req, res) => {
-  const menu = req.query.menu; 
+  const menu = req.query.menu;
   console.log(menu);
   const applications_list = [
-    { status: 1 ,date: "2022-01-01", subject: "Vacation" },
-    {status:2, date: "2022-02-15", subject: "Sick Leave" },
-    { status: 1 ,date: "2022-01-01", subject: "Vacation" },
-    {status:2, date: "2022-02-15", subject: "Sick Leave" },
-    { status: 1 ,date: "2022-01-01", subject: "Vacation" },
-    {status:2, date: "2022-02-15", subject: "Sick Leave" },
-    { status: 1 ,date: "2022-01-01", subject: "Vacation" },
-    {status:2, date: "2022-02-15", subject: "Sick Leave" },
-    { status: 1 ,date: "2022-01-01", subject: "Vacation" },
-    {status:2, date: "2022-02-15", subject: "Sick Leave" },
-    { status: 1 ,date: "2022-01-01", subject: "Vacation" },
-    {status:2, date: "2022-02-15", subject: "Sick Leave" },
-    { status: 1 ,date: "2022-01-01", subject: "Vacation" },
-    {status:2, date: "2022-02-15", subject: "Sick Leave" },
-    { status: 1 ,date: "2022-01-01", subject: "Vacation" },
-    {status:2, date: "2022-02-15", subject: "Sick Leave" }
+    { status: 1, date: "2022-01-01", subject: "Vacation" },
+    { status: 2, date: "2022-02-15", subject: "Sick Leave" },
+    { status: 1, date: "2022-01-01", subject: "Vacation" },
+    { status: 2, date: "2022-02-15", subject: "Sick Leave" },
+    { status: 1, date: "2022-01-01", subject: "Vacation" },
+    { status: 2, date: "2022-02-15", subject: "Sick Leave" },
+    { status: 1, date: "2022-01-01", subject: "Vacation" },
+    { status: 2, date: "2022-02-15", subject: "Sick Leave" },
+    { status: 1, date: "2022-01-01", subject: "Vacation" },
+    { status: 2, date: "2022-02-15", subject: "Sick Leave" },
+    { status: 1, date: "2022-01-01", subject: "Vacation" },
+    { status: 2, date: "2022-02-15", subject: "Sick Leave" },
+    { status: 1, date: "2022-01-01", subject: "Vacation" },
+    { status: 2, date: "2022-02-15", subject: "Sick Leave" },
+    { status: 1, date: "2022-01-01", subject: "Vacation" },
+    { status: 2, date: "2022-02-15", subject: "Sick Leave" },
   ];
-  res.render("index", { title: "Student_Dashboard" , role: "student" , menu, applications_list});
+  const att_data = {
+    subject1: "ranac",
+    attendance1: 10,
+    totalClasses1: 100,
+
+    subject2: "OS",
+    attendance2: 40,
+    totalClasses2: 100,
+
+    subject3: "ADSA",
+    attendance3: 50,
+    totalClasses3: 100,
+
+    subject4: "DBMS",
+    attendance4: 30,
+    totalClasses4: 100,
+
+    subject5: "PC",
+    attendance5: 70,
+    totalClasses5: 100,
+
+    subject6: "SE",
+    attendance6: 90,
+    totalClasses6: 100,
+  };
+  res.render("index", {
+    title: "Student_Dashboard",
+    role: "student",
+    menu,
+    applications_list,
+    att_data,
+  });
 });
 app.get("/admin/dashboard", (req, res) => {
-  const menu = req.query.menu;  
-  res.render("index", { title: "Student_Dashboard" , role: "admin" , menu});
+  const menu = req.query.menu;
+  res.render("index", { title: "Student_Dashboard", role: "admin", menu });
 });
-
-
 
 app.listen(port, (req, res) => {
   console.log("Server running");
